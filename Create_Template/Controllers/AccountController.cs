0101782921db95f0ee087d4f1146b6722ffa5003
kalecaner <canerkale14@gmail.com>
2023-02-08 20:45:15 +0300

    Login View Created

---
+++ Create_Template/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Create_Template.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Create_Template.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                // işlemler
+            }
+            return View(model);
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ Create_Template/Models/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Create_Template.Models
+{
+    public class LoginViewModel
+    {
+       // [Display(Name ="Kullanıcı Adı",Prompt ="Cancan")]
+        [Required(ErrorMessage ="Kullanıcı Adı Zorunlu")]
+        [StringLength(100, ErrorMessage ="Kullanıcı adı en  fazla 100 karakter olabilir")]
+        public string UserName { get; set; }
+
+
+       // [DataType(DataType.Password)]
+        [Required(ErrorMessage ="Parola En az  6 karakterden oluşmalıdır")]
+        [MinLength(6)]
+        [MaxLength(16)]
+        public string Password { get; set; }
+       
+
+       
+    }
+}
