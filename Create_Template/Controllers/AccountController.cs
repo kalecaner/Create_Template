560fd6b3848f3e7f0a93876490de7f4ed30cd5db
kalecaner <canerkale14@gmail.com>
2023-02-10 18:42:38 +0300

    Create encypt

--- Create_Template/Controllers/AccountController.cs
+++ Create_Template/Controllers/AccountController.cs
@@ -1,10 +1,18 @@
-﻿using Create_Template.Models;
+﻿using Create_Template.Entities;
+using Create_Template.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Create_Template.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly DatabaseContext _databaseContext;
+
+        public AccountController(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -24,7 +32,25 @@ namespace Create_Template.Controllers
         {
             if (ModelState.IsValid)
             {
-                //  Register işlemler
+                User user = new()
+                {
+                    Username = model.UserName,
+                    Password = model.Password
+                    // Password Encirpt etmek için netcore.encryp  nugget paketi kullanılabilir.
+                };
+                _databaseContext.Users.Add(user);
+                 int affectedRowCount= _databaseContext.SaveChanges();
+                    if (affectedRowCount==0)
+                {
+                    ModelState.AddModelError("", "Kullanıcı Eklenemedi");
+                }
+                else if (affectedRowCount>0)
+                {
+                    return RedirectToAction(nameof(Login));
+                }
+
+
+
             }
             return View(model);
         }
