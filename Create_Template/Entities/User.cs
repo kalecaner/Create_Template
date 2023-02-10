f965f117f9ce13e33e9aaa57818560e3643da849
kalecaner <canerkale14@gmail.com>
2023-02-10 16:28:14 +0300

    created   DatabaseContext and   User Dbset in entity Folder

---
+++ Create_Template/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Create_Template.Entities
+{
+    [Table("Users")]
+    public class User
+    {
+
+        [Key]
+        public Guid Id { get; set; }
+        [StringLength(50)]
+        public string? Name { get; set; }
+        [StringLength(50)]
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Username { get; set; }
+        public bool Locked { get; set; }=false;
+        public DateTime CreatedAt { get; set; }=DateTime.Now;
+    }
+}
